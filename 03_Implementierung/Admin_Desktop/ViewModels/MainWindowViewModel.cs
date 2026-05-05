b9e04d05cef0f45901fdb48a353c6e6e56a1bf13
Raphael Gafurow <raphgaf2901@gmail.com>
2026-05-05 18:29:40 +0200

    Admin_Desktop: Mitarbeiterliste von API laden und anzeigen

--- 03_Implementierung/Admin_Desktop/ViewModels/MainWindowViewModel.cs
+++ 03_Implementierung/Admin_Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace Admin_Desktop.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Shared;
+namespace Admin_Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private ObservableCollection<Mitarbeiter> _mitarbeiter = new();
+    
+    public MainWindowViewModel()
+    {
+        LadeMitarbeiter();
+    }
+
+    private async void LadeMitarbeiter()
+    {
+        var client = new HttpClient();
+        
+        var liste = await client.GetFromJsonAsync<List<Mitarbeiter>>("http://localhost:5210/api/mitarbeiter");
+        
+        if (liste != null)
+        {
+            Mitarbeiter = new ObservableCollection<Mitarbeiter>(liste);
+        }
+    }
+    
+    
 }
